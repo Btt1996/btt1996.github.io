ee340fb823d7b1e0521b6d4b859a5e19a6709ce0
ahmed taboubi <61942133+Btt1996@users.noreply.github.com>
2023-02-17 05:40:26 +0100

    Update runScript.js

--- js/runScript.js
+++ js/runScript.js
@@ -15,7 +15,7 @@ function runNetflixScript() {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     };
-    const data = { access_token: access_token, profile_id: profile_id };
+    const data = { access_token: access_token };
   
     fetch(url, {
       method: "POST",
