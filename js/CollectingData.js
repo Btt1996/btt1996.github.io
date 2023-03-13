b444bfcb5f315ce5211594c1e3002fe9e9cd6db5
ahmed taboubi <61942133+Btt1996@users.noreply.github.com>
2023-03-13 06:56:40 +0100

    Create CollectingData.js

---
+++ js/CollectingData.js
@@ -0,0 +1,57 @@
+// Get the user's device type from the user agent string
+function getDeviceType(userAgent) {
+  if (/Android/i.test(userAgent)) {
+    return "Android";
+  } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
+    return "iOS";
+  } else if (/Windows Phone/i.test(userAgent)) {
+    return "Windows Phone";
+  } else if (/Windows/i.test(userAgent)) {
+    return "Windows PC";
+  } else if (/Macintosh/i.test(userAgent)) {
+    return "Macintosh";
+  } else if (/Linux/i.test(userAgent)) {
+    return "Linux";
+  } else {
+    return "Unknown";
+  }
+}
+
+// Get the user's internet provider from the user agent string
+function getProvider(userAgent) {
+  const providerRegex = /Mozilla\/5\.0 \((.*?)\)/;
+  const providerMatch = providerRegex.exec(userAgent);
+  if (providerMatch) {
+    return providerMatch[1];
+  } else {
+    return "Unknown";
+  }
+}
+
+// Get the user's location and call the callback function with the tracking data
+function getLocation(callback) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const deviceType = getDeviceType(navigator.userAgent);
+        const provider = getProvider(navigator.userAgent);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const accuracy = position.coords.accuracy;
+        const trackingData = {
+          device_type: deviceType,
+          provider: provider,
+          latitude: latitude,
+          longitude: longitude,
+          accuracy: accuracy
+        };
+        callback(trackingData);
+      },
+      (error) => {
+        showError(error);
+      }
+    );
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
